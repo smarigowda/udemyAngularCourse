f769230bab80d27b0230190329fb87f68a9a8db5
Santosh Marigowda <santosharakere@gmail.com>
2019-11-23 10:03:37 +0000

    assignment 3 in progress...

--- basics-assignment-3-start/src/app/app.component.ts
+++ basics-assignment-3-start/src/app/app.component.ts
@@ -6,4 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  clickCount = [];
+  count = 0;
+  displaySecretPassword = true;
+  onClickHandler() {
+    console.log('button clicked...');
+    this.displaySecretPassword = this.displaySecretPassword ? false : true;
+    console.log('display = ' + this.displaySecretPassword);
+    this.count++;
+    this.clickCount.push(this.count);
+    console.log(this.clickCount);
+  }
 }
